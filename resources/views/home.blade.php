e46499d6ebe5abe6fe815fd64dac706283cba22c
justdata <justdatastage18@gmail.com>
2018-11-21 10:03:22 +0100

    homescreen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,58 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-5">
+            <div class="headerimage">
+            <img src="{{asset('img/head.jpg')}}">
 
+            </div>
         </div>
+
+        <div class="col-md-7">
+            <div class="headertext">
+                <h1>Met fudecu zijn wij beschermd en heeft ons gezin rust!<br>“Onze inboedel is beschermd, onze verplichtingen onder controleen wij kunnen weer sparenvoor bijvoorbeeld een vakantie!!”</h1>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="contenttext">
+            <p>
+                In het leven gebeurd er van alles.<br>
+                Wekomen allemaal voor allerlei verrassingen te staan.<br>
+                Om de continuïteit vanuw huidige levensstandaard te beschermen en onnodige stresste voorkomen,<br>
+                is het slim om uw inboedel (EN INKOMEN) te beschermen.<br>
+                Bescherming vooraf isimmersbeter dan verhelpenachteraf.<br>
+                Fudecuwerkt discreet en effectiefFudecukan u hiermeeblijven helpen!<br>
+                Fudecugeeft U altijd inzage in uw situatie Fudecukan ook helpen als u op dit moment een acuut probleem heeft.<br>
+                U beslistaltijd over de te nemen actie’s.
+            </p>
+        </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="footerimage">
+                <img src="{{asset('img/footer.jpg')}}">
+
+            </div>
+        </div>
+
+        <div class="col-md 8">
+            <div class="footertext">
+                <p>
+                    Meer weten?<br>
+                    Maak een afspraak<br>
+                    Bel: 085-3030791 voor meer info<br>
+                    info@fudecu.nl www.fudecu.nl
+                    Bezoekadres na afspraak:<br>
+                    Kolkweg 20 –10 te Lelystadof<br>
+                    Markerkant 13-10 te Almere
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row justify-content-center">
     </div>
 </div>
 @endsection
